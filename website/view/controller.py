542cdb7912acd213dd9b72af36717e75e31ee5f9
Tejus <tejusrevi@gmail.com>
2020-11-02 14:11:28 -0500

    Added recommendation sysytem

--- website/view/controller.py
+++ website/view/controller.py
@@ -9,7 +9,7 @@ listOfMovies = []
 PASSWORD = "team20"
 DATABASE_NAME = "testDB"
 myclient = pymongo.MongoClient("mongodb+srv://tejus:"+PASSWORD+"@cluster0.ymzum.gcp.mongodb.net/"+DATABASE_NAME+"?retryWrites=true&w=majority")
-mydb = myclient["test"]
+mydb = myclient["testDB"]
 mycol = mydb["testCollection"]
 
 #cursorOfMovies is a cursor object that stores all movies from DB
@@ -17,6 +17,25 @@ cursorOfMovies = mycol.find()
 
 for movie in cursorOfMovies:
     listOfMovies.append(movie)
+
+def getSimiliarMovies(genres,imdbid):
+    eligibleMovies = {}
+    for genre in genres:
+        moviesMatchingGenre = mycol.find({"Genre":genre})
+        for cursor in moviesMatchingGenre:
+            if cursor["imdbID"] != imdbid:
+                if cursor["imdbID"] in eligibleMovies.keys():
+                    eligibleMovies[cursor["imdbID"]] += 1
+                else:
+                    eligibleMovies[cursor["imdbID"]] = 1
+    ls = [ i[0] for i in sorted([(k,v) for k,v in eligibleMovies.items()], key= lambda x: x[1], reverse=True)][0:6]
+    returnList = []
+    for id in ls:
+        returnList.append(mycol.find_one({"imdbID":id}))
+    return returnList
+
+
+
 
 def View(request):
     context = {"listOfMovies":listOfMovies}
@@ -26,10 +45,11 @@ def View(request):
 def navigate_to_movie(request):
     imdbid = request.GET.get('key', '')
     movie = mycol.find_one({ "imdbID": imdbid })
-    context = {"movie":movie}
+    listOfSimiliarMovies = getSimiliarMovies(movie["Genre"], imdbid)
+    context = {"movie":movie, "similiarMovies":listOfSimiliarMovies}
     response = render(request, 'movie.html', context)
-
     return response
+
 def view_database(request):
     context = {"listOfMovies":listOfMovies}
     response = render(request, 'database.html', context)
--- website/view/urls.py
+++ website/view/urls.py
@@ -3,6 +3,6 @@ from . import controller
 
 urlpatterns = [
     path('', controller.View, name = 'home-page'),
-    path('navigate_to_movie', controller.navigate_to_movie, name = 'navigate_to_movie'),
+    path('movie', controller.navigate_to_movie, name = 'navigate_to_movie'),
     path('view_database', controller.view_database, name = 'view_database'),
 ]
